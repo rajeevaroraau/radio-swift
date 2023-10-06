f444ba68015d66f5662abf098c1685fff4e89d03
Marcin Wolski <martinwolski04@gmail.com>
2023-10-06 16:26:38 +0200

    Initial Commit

---
+++ radio/radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  radioApp.swift
+//  radio
+//
+//  Created by Marcin Wolski on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
