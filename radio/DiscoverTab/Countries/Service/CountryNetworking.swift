e341abbe29846c14b568e64ee7bf716f7a7cd685
Marcin Wolski <martinwolski04@gmail.com>
2024-05-26 19:15:50 +0200

    Improve error handling in CountryNetworking.swift

--- radio/DiscoverTab/Countries/Service/CountryNetworking.swift
+++ radio/DiscoverTab/Countries/Service/CountryNetworking.swift
@@ -18,19 +18,32 @@ class CountryNetworking {
     
     private let url: URL
     
+    enum NetworkingError: Error {
+        case invalidURL
+        case requestFailed(Error)
+        case invalidResponse
+        case decodingError(Error)
+    }
+    
     func requestCountries() async throws -> [Country] {
         os_signpost(.begin, log: pOI, name: "CountryNetworking.requestCountries()")
+        
         do {
             Logger.countryNetworking.notice("Fetching from \(self.url)...")
-            let (data, _) = try await URLSession.shared.data(from: url)
+            let (data, response) = try await URLSession.shared.data(from: url)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                throw NetworkingError.invalidResponse
+            }
+            
             let countries = try JSONDecoder().decode([Country].self, from: data)
+            
             os_signpost(.end, log: pOI, name: "CountryNetworking.requestCountries()")
             return countries.reversed()
         } catch {
             os_signpost(.end, log: pOI, name: "CountryNetworking.requestCountries()")
             Logger.countryNetworking.error("Request error: \(error)")
-            return []
+            throw NetworkingError.requestFailed(error)
         }
     }
-    
 }
